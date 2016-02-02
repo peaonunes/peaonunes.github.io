e459163e631c4dda0d624df5611d0c185e00fb83
Rafael Nunes G. da Silveira <rngs@cin.ufpe.br>
2016-02-01 22:06:48 -0300

    #01 - First play around

---
+++ js/onClickAction.js
@@ -0,0 +1,8 @@
+function changeDiv(i){
+   if (i == 3)
+       document.getElementById("main").innerHTML = document.getElementById("mainProjects").innerHTML;
+   else if (i == 2)
+       document.getElementById("main").innerHTML = document.getElementById("mainBlog").innerHTML;
+   else if (i == 1)
+       document.getElementById("main").innerHTML = document.getElementById("mainResume").innerHTML;
+}
