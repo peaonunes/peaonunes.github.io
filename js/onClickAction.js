ba9ac0ea644341cb39c1395b0acc3f13b90e05ea
Rafael Nunes G. da Silveira <rngs@cin.ufpe.br>
2016-02-14 22:29:22 -0300

    Improving some stuff + resume

--- js/onClickAction.js
+++ js/onClickAction.js
@@ -1,8 +1,10 @@
 function changeDiv(i){
-   if (i == 3)
-       document.getElementById("main").innerHTML = document.getElementById("mainProjects").innerHTML;
-   else if (i == 2)
-       document.getElementById("main").innerHTML = document.getElementById("mainBlog").innerHTML;
-   else if (i == 1)
-       document.getElementById("main").innerHTML = document.getElementById("mainResume").innerHTML;
+	if (i == 3)
+		document.getElementById("main").innerHTML = document.getElementById("mainProjects").innerHTML;
+	else if (i == 2)
+		document.getElementById("main").innerHTML = document.getElementById("mainBlog").innerHTML;
+	else if (i == 1)
+		document.getElementById("main").innerHTML = document.getElementById("mainResume").innerHTML;
+	else
+		document.getElementById("main").innerHTML = document.getElementById("mainSite").innerHTML;
 }
