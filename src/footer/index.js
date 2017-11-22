8071ad184e724c2cf508c991221a0cdf6c0bea1c
Rafael Nunes G. da Silveira <rafaelngds@gmail.com>
2017-11-21 23:33:17 -0300

    feat(docs): build for docs

--- src/footer/index.js
+++ src/footer/index.js
@@ -8,10 +8,10 @@ class Footer extends React.Component {
         <p className="light courier-new">> add credits</p>
         <p className="light small-text">
           This is my third attemp on building a personal site.<br/>
-          This version was made with <a target="_blank" href="http://materializecss.com/">MaterializeCSS</a>,
-          <a target="_blank" href="https://github.com/michalsnik/aos"> AOS</a>,
-          <a target="_blank" href="https://reactjs.org/"> React</a> and spikes of inspiration.
-          Font Awesome by Dave Gandy - <a target="_blank" href="http://fontawesome.io">http://fontawesome.io</a>.
+          This version was made with <a target="_blank" rel="noopener noreferrer" href="http://materializecss.com/">MaterializeCSS</a>,
+          <a target="_blank" rel="noopener noreferrer" href="https://github.com/michalsnik/aos"> AOS</a>,
+          <a target="_blank" rel="noopener noreferrer" href="https://reactjs.org/"> React</a> and spikes of inspiration.
+          Font Awesome by Dave Gandy - <a target="_blank" rel="noopener noreferrer" href="http://fontawesome.io">http://fontawesome.io</a>.
         </p>
       </div>
     )
