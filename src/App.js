3b096b442309a61f1e3f50fe039d802931b941a2
Rafael Nunes G. da Silveira <rafaelngds@gmail.com>
2019-09-04 17:19:54 +1000

    refactor(App): split in smaller components, reuse common, clean stuff

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import '../node_modules/aos/dist/aos.css' 
+import '../node_modules/aos/dist/aos.css'
 import WorkExperience from './working'
 import Education from './education'
 import Projects from './projects'
@@ -6,6 +6,7 @@ import Teaching from './teaching'
 import Footer from './footer'
 import About from './about'
 import AOS from 'aos'
+
 import './App.css'
 
 import React, { Component } from 'react'
@@ -17,7 +18,7 @@ class App extends Component {
 
     this.state = {
       finishedImporting: false,
-      loadingBar: "[===>]",
+      loadingBar: '[===>]',
       counter: 0
     }
 
@@ -40,11 +41,9 @@ class App extends Component {
   render() {
     return (
       <div className="container courier-new">
-        {
-          this.state.finishedImporting
+        {this.state.finishedImporting
           ? this.renderContent()
-          : this.renderLoaderWrapper()
-        }
+          : this.renderLoaderWrapper()}
       </div>
     )
   }
@@ -54,9 +53,9 @@ class App extends Component {
       <div>
         <About />
         <WorkExperience />
-        <Projects />
-        <Teaching />
         <Education />
+        <Teaching />
+        <Projects />
         <Footer />
       </div>
     )
@@ -69,13 +68,16 @@ class App extends Component {
   }
 
   tick() {
-    if (this.state.counter === 1000) {
+    if (this.state.counter === 800) {
       this.setState({ finishedImporting: true })
       clearInterval(this.timer)
     } else {
       this.setState({
         counter: this.state.counter + 10,
-        loadingBar: this.state.loadingBar.slice(0, 3) + "=" + this.state.loadingBar.slice(3)
+        loadingBar:
+          this.state.loadingBar.slice(0, 3) +
+          '=' +
+          this.state.loadingBar.slice(3)
       })
     }
   }
@@ -96,35 +98,37 @@ class App extends Component {
   renderLoading() {
     return (
       <div>
-        { this.renderWelcome() }
-        { this.renderName() }
-        <span># {this.state.counter}/1000 useless, but cool, animation loader...</span><br/>
+        <div className="welcome-subtitle">
+          {welcome1}
+          <br />
+          {welcome2}
+          <br />
+          {welcome3}
+          <br />
+          {welcome4}
+          <br />
+          {welcome5}
+          <br />
+          {welcome6}
+          <br />
+        </div>
+        <div>
+          {str1}
+          <br />
+          {str2}
+          <br />
+          {str3}
+          <br />
+          {str4}
+          <br />
+          {str5}
+          <br />
+        </div>
+        <span>
+          # {this.state.counter}/1000 useless, but cool, animation loader...
+        </span>
+        <br />
         {this.state.loadingBar}
-      </div>
-    )
-  }
-
-  renderWelcome() {
-    return (
-      <div className="welcome-subtitle">
-        {welcome1}<br />
-        {welcome2}<br />
-        {welcome3}<br />
-        {welcome4}<br />
-        {welcome5}<br />
-        {welcome6}<br />
-      </div>
-    )
-  }
-
-  renderName() {
-    return (
-      <div>
-        {str1}<br />
-        {str2}<br />
-        {str3}<br />
-        {str4}<br />
-        {str5}<br />
       </div>
     )
   }
@@ -132,15 +136,15 @@ class App extends Component {
 
 export default App
 
-const str1 = "     ____        ____           __   _   __                     "
-const str2 = "    / __ |____ _/ __/___ ____  / /  / | / /_  ______  ___  _____"
-const str3 = "   / /_/ / __ `/ /_/ __ `/ _ |/ /  /  |/ / / / / __ |/ _ |/ ___/"
-const str4 = "  / _, _/ /_/ / __/ /_/ /  __/ /  / /|  / /_/ / / / /  __(__  ) "
-const str5 = " /_/ |_||__,_/_/  |__,_/|___/_/  /_/ |_/|__,_/_/ /_/|___/____/  "
+const str1 = '     ____        ____           __   _   __                     '
+const str2 = '    / __ |____ _/ __/___ ____  / /  / | / /_  ______  ___  _____'
+const str3 = '   / /_/ / __ `/ /_/ __ `/ _ |/ /  /  |/ / / / / __ |/ _ |/ ___/'
+const str4 = '  / _, _/ /_/ / __/ /_/ /  __/ /  / /|  / /_/ / / / /  __(__  ) '
+const str5 = ' /_/ |_||__,_/_/  |__,_/|___/_/  /_/ |_/|__,_/_/ /_/|___/____/  '
 
-const welcome1 = "                __                             __      "
-const welcome2 = " _      _____  / /________  ____ ___  ___     / /_____ "
-const welcome3 = "| | /| / / _ |/ / ___/ __ |/ __ `__ |/ _ |   / __/ __ |"
-const welcome4 = "| |/ |/ /  __/ / /__/ /_/ / / / / / /  __/  / /_/ /_/ /"
-const welcome5 = "|__/|__/|___/_/|___/|____/_/ /_/ /_/|___/   |__/|____/ "
-const welcome6 = "                                                       "
+const welcome1 = '                __                             __      '
+const welcome2 = ' _      _____  / /________  ____ ___  ___     / /_____ '
+const welcome3 = '| | /| / / _ |/ / ___/ __ |/ __ `__ |/ _ |   / __/ __ |'
+const welcome4 = '| |/ |/ /  __/ / /__/ /_/ / / / / / /  __/  / /_/ /_/ /'
+const welcome5 = '|__/|__/|___/_/|___/|____/_/ /_/ /_/|___/   |__/|____/ '
+const welcome6 = '                                                       '
--- src/footer/index.js
+++ src/footer/index.js
@@ -7,15 +7,44 @@ class Footer extends React.Component {
       <div className="row">
         <p className="light courier-new">> add credits</p>
         <p className="light small-text">
-          This is my third attemp on building a personal site.<br/>
-          This version was made with <a target="_blank" rel="noopener noreferrer" href="http://materializecss.com/">MaterializeCSS</a>,
-          <a target="_blank" rel="noopener noreferrer" href="https://github.com/michalsnik/aos"> AOS</a>,
-          <a target="_blank" rel="noopener noreferrer" href="https://reactjs.org/"> React</a> and spikes of inspiration.
-          Font Awesome by Dave Gandy - <a target="_blank" rel="noopener noreferrer" href="http://fontawesome.io">http://fontawesome.io</a>.
+          This is my third attemp on building a personal site. I will probably
+          rewrite it in 2019 since I really know better way of doing it.
+          <br />
+          This version was made with{' '}
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="http://materializecss.com/">
+            MaterializeCSS
+          </a>
+          ,
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://github.com/michalsnik/aos">
+            {' '}
+            AOS
+          </a>
+          ,
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://reactjs.org/">
+            {' '}
+            React
+          </a>{' '}
+          and spikes of inspiration. Font Awesome by Dave Gandy -{' '}
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="http://fontawesome.io">
+            http://fontawesome.io
+          </a>
+          .
         </p>
       </div>
     )
   }
 }
 
-export default Footer;
+export default Footer
