c42d5014774c2f8d2649d394bc8e0b67fe5c9be2
Rafael Nunes G. da Silveira <rafaelngds@gmail.com>
2019-09-04 17:19:33 +1000

    refactor(education): split in smaller components, reuse common, clean stuff

---
+++ src/working/Content.js
@@ -0,0 +1,22 @@
+import PropTypes from 'prop-types'
+import React from 'react'
+
+const Content = ({ onClick, position, href, company, period }) => (
+  <div>
+    <b>{position}</b> @
+    <a target="_blank" href={href} onClick={e => onClick(e, href, company)}>
+      <span className="light job-title"> {company}</span>
+    </a>
+    <p className="job-period">{period}</p>
+  </div>
+)
+
+Content.propTypes = {
+  position: PropTypes.string.isRequired,
+  href: PropTypes.string,
+  company: PropTypes.string.isRequired,
+  period: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired
+}
+
+export default Content
--- src/working/index.js
+++ src/working/index.js
@@ -1,124 +1,107 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React from 'react'
+import PropTypes from 'prop-types'
 
-import inloco from "./assets/inloco.png";
-import suati from "./assets/suati.png";
-import "./assets/working.css";
+import inloco from './assets/inloco.png'
+import suati from './assets/suati.png'
+import stig from './assets/stig.png'
+import './assets/working.css'
+
+import Skills from './../common/Skills'
+
+import Content from './Content'
+import Image from '../common/Image'
 
 class WorkingExperience extends React.Component {
   static contextTypes = {
     mixpanel: PropTypes.object.isRequired
-  };
+  }
 
   render() {
     return (
       <div className="row" data-aos="fade-in">
         <h5 className="light courier-new">> describe experience</h5>
-        {this.renderJobs()}
-      </div>
-    );
-  }
-
-  renderJobs() {
-    return JOBS.map((job, index) => {
-      return (
-        <div key={`${job.company}_${index}`} className="row job-row">
-          {this.renderImage(job)}
-          <div className="col s12 l10 m10 job-content">
-            <div className="center hide-on-med-and-up">
-              {this.renderContent(job)}
-              {this.renderSkills(job)}
-            </div>
-            <div className="hide-on-small-only show-on-medium-and-up">
-              {this.renderContent(job)}
-              {this.renderSkills(job)}
-            </div>
-          </div>
-        </div>
-      );
-    });
-  }
-
-  renderImage(job) {
-    return (
-      <div className="col s12 l2 m2 center">
-        <img src={job.img} className="circle job-img" alt={job.company} />
-      </div>
-    );
-  }
-
-  renderContent(job) {
-    return (
-      <div>
-        <b>{job.position}</b> @
-        <a
-          target="_blank"
-          href={job.href}
-          onClick={e => this.trackClick(e, job.href, job.company)}
-        >
-          <span className="light job-title"> {job.company}</span>
-        </a>
-        <p className="job-date">{job.date}</p>
-      </div>
-    );
-  }
-
-  trackClick(event, href, company) {
-    this.context.mixpanel.track("job_link_clicked", {
-      link: href,
-      company: company
-    });
-  }
-
-  renderSkills(job) {
-    return (
-      <div>
-        {job.skills.map((skillName, index) => {
+        {JOBS.map((job, index) => {
           return (
-            <div key={`${skillName}_${index}`} className="chip">
-              {skillName}
+            <div key={`${job.company}_${index}`} className="row job-row">
+              <Image src={job.img} alt={job.company} />
+              <div className="col s12 l10 m10 job-content">
+                <div className="center hide-on-med-and-up">
+                  <Content
+                    onClick={this.trackClick}
+                    position={job.position}
+                    href={job.href}
+                    period={job.period}
+                    company={job.company}
+                  />
+                  <Skills skills={job.skills} />
+                </div>
+                <div className="hide-on-small-only show-on-medium-and-up">
+                  <Content
+                    onClick={this.trackClick}
+                    position={job.position}
+                    href={job.href}
+                    period={job.period}
+                    company={job.company}
+                  />
+                  <Skills skills={job.skills} />
+                </div>
+              </div>
             </div>
-          );
+          )
         })}
       </div>
-    );
+    )
+  }
+
+  trackClick = (_, href, company) => {
+    this.context.mixpanel.track('job_link_clicked', {
+      link: href,
+      company: company
+    })
   }
 }
 
 const JOBS = [
   {
-    company: "In Loco",
-    href: "https://www.inloco.com.br",
+    company: 'In Loco',
+    href: 'https://www.inloco.com.br',
     img: inloco,
-    position: "Software Engineer",
-    date: "Mar, 2017 - Today",
+    position: 'Software Engineer & Frontend Chapter Lead',
+    period: 'Mar, 2017 - Aug, 2019',
     skills: [
-      "Java",
-      "React",
-      "GraphQL",
-      "Ruby on Rails",
-      "Kubernetes",
-      "Kafka",
-      "gRPC"
+      'React',
+      'GraphQL',
+      'Ruby on Rails',
+      'Java',
+      'Kubernetes',
+      'Kafka',
+      'gRPC'
     ]
   },
   {
-    company: "Suati",
-    href: "http://suati.com.br/",
+    company: 'Suati',
+    href: 'http://suati.com.br/',
     img: suati,
-    position: "Software Engineer Intern",
-    date: "Apr, 2015 - Jan, 2016",
-    skills: ["C#", "TFS", "Powershell"]
+    position: 'Software Engineer Intern',
+    period: 'Apr, 2015 - Jan, 2016',
+    skills: ['C#', 'TFS', 'Powershell']
   },
   {
-    company: "PET-Informática",
-    href: "https://pet-informatica.github.io/",
+    company: 'PET-Informática',
+    href: 'https://pet-informatica.github.io/',
     img:
-      "https://res.cloudinary.com/dkbuneg9h/image/upload/v1477079274/pet/pet_wn9jqn.png",
-    position: "Member",
-    date: "Mar, 2013 - Jan, 2017",
-    skills: ["Unity", "HTML", "C#", "CSS", "JavaScript"]
+      'https://res.cloudinary.com/dkbuneg9h/image/upload/v1477079274/pet/pet_wn9jqn.png',
+    position: 'Member',
+    period: 'Mar, 2013 - Jan, 2017',
+    skills: ['Unity', 'HTML', 'C#', 'CSS', 'JavaScript']
+  },
+  {
+    company: 'Sig App',
+    img: stig,
+    position: 'Project Manager & Co-Founder',
+    period: 'Jan, 2013 - Apr, 2014',
+    skills: ['Scrum', 'Lean Startup']
   }
-];
+]
 
-export default WorkingExperience;
+export default WorkingExperience
